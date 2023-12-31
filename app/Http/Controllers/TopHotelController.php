585925e3aaa5a1768e56aa1b340257e0c1b0b534
eslamabdelbasset1 <eslamabdelbasset1@gmail.com>
2023-12-31 17:40:20 +0200

    remove usleep from topHotelsCintroller

--- app/Http/Controllers/TopHotelController.php
+++ app/Http/Controllers/TopHotelController.php
@@ -13,8 +13,6 @@ class TopHotelController extends Controller
      */
     public function index()
     {
-        // Simulate a delay of 3 seconds
-        sleep(3);
         $bestHotels = TopHotel::all();
         return TopHotelResourceCollection::collection($bestHotels);
     }
